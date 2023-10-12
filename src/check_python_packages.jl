17a5e6d7123e981b3cf32886fef3d2b22faa8c1f
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-12 13:58:38 +1300

    wrinkles

--- src/check_python_packages.jl
+++ src/check_python_packages.jl
@@ -7,7 +7,7 @@ function check_python_packages()
         return [p not in sys.modules for p in checked_package_names]
     """
     required = py"req"(checked_package_names)
-    Conda.pip_interop(true, Conda.PYTHONDIR) 
+    Conda.pip_interop(true, string("..",Conda.PYTHONDIR) )
     for req_package in checked_package_names[required]
         println("installing:  ", req_package)
         Conda.pip("install", req_package, Conda.PYTHONDIR)
