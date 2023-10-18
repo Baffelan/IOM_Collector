41c10e5bfcd37d9de599d9bb6500c81e75e8086b
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-19 10:07:02 +1300

    comment out bad syntax

--- src/queryBuilder/query_format.jl
+++ src/queryBuilder/query_format.jl
@@ -1,24 +1,24 @@
 function query_format()
 
-    "query": {
-        "$query": {
-          "$and": [
-            {
-              "dateStart": "2017-04-22",
-              "dateEnd": "2017-04-22",
-              "categoryUri": "dmoz/Business"
-            },
-            {
-              "$or": [
-                {
-                  "conceptUri": "http://en.wikipedia.org/wiki/Barack_Obama"
-                },
-                {
-                  "keyword": "Trump"
-                }
-              ]
-            }
-          ]
-        }
-      },
+    # "query": {
+    #     "$query": {
+    #       "$and": [
+    #         {
+    #           "dateStart": "2017-04-22",
+    #           "dateEnd": "2017-04-22",
+    #           "categoryUri": "dmoz/Business"
+    #         },
+    #         {
+    #           "$or": [
+    #             {
+    #               "conceptUri": "http://en.wikipedia.org/wiki/Barack_Obama"
+    #             },
+    #             {
+    #               "keyword": "Trump"
+    #             }
+    #           ]
+    #         }
+    #       ]
+    #     }
+    #   },
 end
